4621e34220d93c5001ccea474137af5ee7c13fcb
S4AK4N <160442784+S4AK4N@users.noreply.github.com>
2024-10-29 16:23:33 +0900

    chap9を修了(テンプレートを分割してルーティングを行う)

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+savas  = "さばす"
+
 @app.route("/")
 def hello_world():
 
-    savas  = "さばす"
+    
 
     return render_template("index.html",savas = savas)
 
@@ -13,4 +15,13 @@ def about():
     nemui = "ねむい"
     return render_template("another.html",nemui = nemui)
 
+@app.route("/walk")
+def walk():
+    message = savas + "は歩いてるよ～"
+    return render_template("walk.html",savas = savas , message = message)
+
+@app.route("/eat")
+def eat():
+    foodlist = ["りんご","みかん","メロン"]
+    return render_template("eat.html",savas=savas , foodlist = foodlist)
 #TODO:チャプター5を進める(HTMLに変数受け渡し)
