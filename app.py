10d7fc5f0f90e0215e2fb42f1696966c817df1de
S4AK4N <160442784+S4AK4N@users.noreply.github.com>
2024-10-30 14:21:25 +0900

    フォームで投稿した内容を別ページで表示できるようにした

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template,request
 app = Flask(__name__)
 
-savas  = "さばす"
-
+names = ["さばす","TanakaMasahiko","KawaiShinzoV2"]
+savas = "さばす"
 # NOTE:routeにアクセスした時
 @app.route("/")
 def top():
@@ -13,40 +13,25 @@ def top():
 @app.route("/form")
 def show():
 
-    return render_template("form.html",savas = savas)
+
+    return render_template("form.html",names = names)
 
 # NOTE:form.htmlでtext,nameがsubmitで押された時,/resultを走らせるようにした
-@app.route("/result",methods =["GET","POST"])
+@app.route("/result", methods=["GET", "POST"])
 def result():
-    
-    # POSTメソッドが戻り値だった時 
-    if request.method == "POST":
 
+    # POSTメソッドの場合
+    if request.method == "POST":
         name = request.form["name"]
-        text = request.form["text"]
-    #GETメソッドだった時
+        text = request.form["article"]  # `text`ではなく`article`に変更
+    # GETメソッドの場合
     else:
         name = request.args.get("name")
         text = request.args.get("text")
-    return render_template("form.html",savas=savas,name=name,text=text)
+
+    return render_template("form_result.html", name=name, text=text)
 
 
-@app.route("/about")
-def about():
-    nemui = "ねむい"
-    return render_template("another.html",nemui = nemui)
-
-# NOTE:walkにアクセスした時
-@app.route("/walk")
-def walk():
-    message = savas + "は歩いてるよ～"
-    return render_template("walk.html",savas = savas , message = message)
-
-#NOTE:/eatにアクセスした時,以下の内容を送信した,eat.htmlをブラウザに表示
-@app.route("/eat")
-def eat():
-    foodlist = ["りんご","みかん","メロン"]
-    return render_template("eat.html",savas=savas , foodlist = foodlist)
 
 
 #TODO:チャプター5を進める(HTMLに変数受け渡し)
